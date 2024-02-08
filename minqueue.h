f0ad66712fd03ba27800d939b26bb7354556245b
William Nguyen <nguyen_p3@denison.edu>
2024-02-08 15:25:22 -0500

    Added methods for heap and minqueue

---
+++ minqueue.h
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef MINQUEUE_H
+#define MINQUEUE_H
+
+template <class T>
+class Minqueue{
+    private:
+        parent( int i );
+        left( int i );
+        right( int i );
+
+    public:
+                    Minqueue            ( void );
+                    Minqueue            (T* A, int n);
+                    ~Minqueue           ( void );
+        void        insert              ( const T x ):
+        T           min                 ( void );
+        T           extract_min         ( void );
+        void        decrease_key        ( int i, T k );
+        void        min_heapify         (int i);
+        void        build_min_heap      ( void );
+        void        heapsort            ( const T& A );
+        string      to_string           ( void );
+};
+
+#include "minqueue.cpp"
+#endif
+
