c3ecc39947f918589b46f55cc6f39eb627ad09ef
William Nguyen <nguyen_p3@denison.edu>
2024-02-21 12:55:06 -0500

    wrote main.cpp

--- main.cpp
+++ main.cpp
@@ -8,3 +8,25 @@
 #include "usecase.cpp"
 
 using namespace std;
+
+int main(){
+    int num[8] = {1,2,3,4,5,6,-1,-2};
+    string window_result = sliding_window(num, 8, 4);
+    if (window_result != "1 2 3 -1 -2")
+    {
+        cout << "Incorrect sliding window result. Expected 1 2 3 -1 -2 but got : " << window_result << endl;
+    }
+    window_result = sliding_window(num, 8, 5);
+    if (window_result != "1 2 -1 -2")
+    {
+        cout << "Incorrect sliding window result. Expected 1 2 -1 -2 but got : " << window_result << endl;
+    }
+    window_result = sliding_window(num, 8, 8);
+    if (window_result != "-2")
+    {
+        cout << "Incorrect sliding window result. Expected -2 but got : " << window_result << endl;
+    }
+
+    cout << "Testing completed" << endl;
+    return 0;
+}
